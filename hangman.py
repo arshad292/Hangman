5457c8ca314ce688c6040cf95e04474dbd5f82e8
arshad292 <arshad.kma@gmail.com>
2022-08-30 21:08:07 +0530

    final

--- hangman.py
+++ hangman.py
@@ -15,11 +15,13 @@ def hangman():
     alphabet = set(string.ascii_uppercase)
     used_letters = set() #what the user has guessed
 
+    lives = 6
+
     #getting user input
-    while len(word_letters) > 0:
+    while len(word_letters) > 0 and lives>0 :
         #letters used
         #''.join(['a', 'b', 'cd' ]) --> 'a b cd'
-        print('You have used these letters: ', ' '.join(used_letters))
+        print('You have',lives,'lives left and you have used these letters: ', ' '.join(used_letters))
         
         #what current word is(ie w - R D)
         word_list = [letter if letter in used_letters else '-' for letter in word]
@@ -30,12 +32,21 @@ def hangman():
             used_letters.add(user_letter)
             if user_letter in word_letters:
                 word_letters.remove(user_letter)
+
+            else:
+                lives = lives - 1
+                print('Letter is not in word. ')
         
         elif user_letter in used_letters:
             print("You have already used that character.Please try again. ")
         else:
             print("Invalid character. Please try again. ")
-    #gets here when len(word_letters) == 0
+    #gets here when len(word_letters) == 0 OR when lives == 0
+
+    if lives == 0:
+        print('You died, sorry. The word was', word)
+    else:
+        print('Invalid character. Please try again.')
 hangman()
 
 
